6897d13fb6bbd50801f3694b0158f3783995d891
Melquisedeque <melquisedequevital@gmail.com>
2026-06-05 00:23:30 -0300

    ajusta import e vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-export default defineConfig({
-  root: 'src',
-  base: "/Movement",
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(process.cwd(), "src/index.html"),
-        "front-body": resolve(process.cwd(), "src/front-body.html"),
-        "back-body": resolve(process.cwd(), "src/back-body.html"),
+export default defineConfig(({ command }) => {
+  return {
+    root: "src",
+    base: command === "build" ? "/Movement/" : "/",
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(process.cwd(), "src/index.html"),
+          "front-body": resolve(process.cwd(), "src/front-body.html"),
+          "back-body": resolve(process.cwd(), "src/back-body.html"),
+        },
       },
     },
-  },
+  };
 });
