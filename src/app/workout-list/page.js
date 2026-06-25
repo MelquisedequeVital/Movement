964209899b5e4e36eea6b318a915ff2e843456f6
Melquisedeque <melquisedequevital@gmail.com>
2026-06-24 23:31:38 -0300

    chore: add function remove workout to workoutList

--- src/app/workout-list/page.js
+++ src/app/workout-list/page.js
@@ -44,6 +44,14 @@ function WorkoutListContent() {
       .catch((err) => console.error(err));
   };
 
+  const removeWorkout = (id) =>  {
+    fetch(`${API_URL}/${id}`, {
+      method: "DELETE"
+    })
+    .then(() => setWorkouts((old) => old.filter((workout) => workout.id !== id)))
+    .catch((err) => console.error(err));
+  }
+
   return (
     <div className="max-w-5xl mx-auto p-6 mt-6">
       <div className="flex justify-between items-center mb-8">
@@ -63,7 +71,7 @@ function WorkoutListContent() {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {workouts.map((workout) => (
-            <WorkoutCard key={workout.id} treino={workout} bodyPart={bodyPart} />
+            <WorkoutCard removeWorkout={removeWorkout} key={workout.id} treino={workout} bodyPart={bodyPart} />
           ))}
         </div>
       )}
