fd6c6ee0eea4b314f699175e1337c7b5c8db427e
Melquisedeque <melquisedequevital@gmail.com>
2026-06-25 03:05:43 -0300

    build: retira o output export do next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  output: 'export', 
-  images: {
-    unoptimized: true, 
-  },
-  reactCompiler: true,
+    /* Você pode deixar configurações vazias ou adicionar outras regras futuramente */
 };
 
 export default nextConfig;
